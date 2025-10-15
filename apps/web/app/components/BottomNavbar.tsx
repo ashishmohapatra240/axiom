d3fe4a513d75e0a65f4f9699d8daf046f35fb7c1
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-10-15 22:53:11 +0530

    fix(web): adjust z-index of BottomNavbar

--- apps/web/app/components/BottomNavbar.tsx
+++ apps/web/app/components/BottomNavbar.tsx
@@ -3,7 +3,7 @@ import { FiSettings, FiStar, FiTrendingUp } from "react-icons/fi";
 
 const BottomNavbar = () => {
   return (
-    <div className="sticky top-[52px] sm:top-[64px] z-40 bg-[#06070B] border-b border-gray-900">
+    <div className="sticky top-[52px] sm:top-[64px] z-10 bg-[#06070B] border-b border-gray-900">
       <div className="flex space-x-2 sm:space-x-2.5 py-1.5 sm:py-2 ml-2 sm:ml-4 overflow-x-auto scrollbar-hide">
         <FiSettings className="w-3 h-3 sm:w-3.5 sm:h-3.5 text-[#787A87] flex-shrink-0" />
         <p className="text-gray-600 text-[10px] flex-shrink-0">|</p>
